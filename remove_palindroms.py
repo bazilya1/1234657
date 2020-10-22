bdbffa6b69a52c16d47e2c3e2b04630f5480abda
bazilya1 <73291655+bazilya1@users.noreply.github.com>
2020-10-22 16:27:31 +0300

    Add files via upload

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -2,6 +2,6 @@ def remove_palindroms(spells):
     local(a)
     a = 0
     for i in spells:
-        if (i.lower()).split() == (i[::-1].lower()).split():
+        if i.lower().split() == i[::-1].lower().split():
             spells.pop(a)
         a += 1
