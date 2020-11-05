f063ce32b44bc1cc711a21fb681047e3917d09ce
bazilya1 <73291655+bazilya1@users.noreply.github.com>
2020-11-05 16:27:49 +0300

    Add files via upload

---
+++ Button.py
@@ -0,0 +1,16 @@
+class Button:
+    global a
+    a = 0
+    def click(self):
+        global a
+        a += 1
+        print(a)
+
+    def click_count(self):
+        global a
+        a -= 1
+        print(a)
+
+    def reset(self):
+        global a
+        a = 0
